12d8f5b4ed7d445e226fd00866866e04134a07a9
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-09 16:32:55 +0200

    Updated Distionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,6 +6,7 @@ import Result from "./Result";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("hello");
   const [result, setResult] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   //API docu: https://www.shecodes.io/learn/apis/dictionary
   function handleResponse(response) {
@@ -16,27 +17,36 @@ export default function Dictionary() {
     event.preventDefault();
     setKeyword(event.target.value);
   }
-
   function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
+
+  function search() {
     let apiKey = "636ft3f4ca7b895f0259dd71a1354d0o";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
     axios.get(apiUrl).then(handleResponse);
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Search for ...."
-          onChange={handleChange}
-        ></input>
-        <button type="submit">
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
-      </form>
-      <Result result={result} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Search for ...."
+            onChange={handleChange}
+          ></input>
+          <button type="submit">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+        <Result result={result} />
+      </div>
+    );
+  } else {
+    setLoaded(true);
+    search();
+    return null;
+  }
 }
